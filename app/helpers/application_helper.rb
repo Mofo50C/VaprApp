9e9c47c1c30afdccec4b512a83f6e2da1b61efdc
Mofo50C <mforoughi@rocketmail.com>
2021-12-02 11:51:22 +0000

    refactored some view grid elements, and some localisation stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def format_total_price(total)
+        t('orders.total') + ": " + format_price(total)
+    end
+
+    def format_price(price)
+        number_to_currency(price, unit: "£")
+    end
 end
--- app/helpers/orders_helper.rb
+++
@@ -1,5 +0,0 @@
-module OrdersHelper
-    def format_total_price(total)
-        "#{t('.total')}: #{t('currency')}%.2f" % total
-    end
-end
